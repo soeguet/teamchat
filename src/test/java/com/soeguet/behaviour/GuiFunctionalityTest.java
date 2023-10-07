b86c029a0b895deb3621fefbc4e8bc978e78cf58
soeguet <osman_s@live.de>
2023-10-07 18:21:06 +0200

    "Add new GuiFunctionalityTest class
    This commit introduces a new test class named GuiFunctionalityTest.java under com.soeguet.behaviour. This is part of our ongoing efforts to improve test coverage and verify the behavior of our GUI features. The new class currently includes a single test method, onMessage(), which is yet to be fully implemented. As Mockito is used in commented out code, incoming commits are expected to implement mock-based unit tests for better isolation of GUI functionality."

---
+++ src/test/java/com/soeguet/behaviour/GuiFunctionalityTest.java
@@ -0,0 +1,18 @@
+package com.soeguet.behaviour;
+
+import com.soeguet.gui.main_frame.MainFrameInterface;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GuiFunctionalityTest {
+
+    @Test
+    void onMessage() {
+
+//        MainFrameInterface mainFrame = Mockito.spy(MainFrameInterface.class);
+//        GuiFunctionality guiFunctionality = new GuiFunctionality(mainFrame);
+//        guiFunctionality.onMessage("test");
+    }
+}
