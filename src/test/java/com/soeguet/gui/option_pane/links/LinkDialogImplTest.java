9345a5e332752f7ed1ced64c82448c996eef91a3
soeguet <osman_s@live.de>
2024-01-14 18:17:15 +0100

    fixed failing test

--- src/test/java/com/soeguet/gui/option_pane/links/LinkDialogImplTest.java
+++ src/test/java/com/soeguet/gui/option_pane/links/LinkDialogImplTest.java
@@ -1,9 +1,7 @@
 package com.soeguet.gui.option_pane.links;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
-
 import com.soeguet.gui.option_pane.links.dtos.MetadataStorageRecord;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
@@ -12,32 +10,34 @@ class LinkDialogImplTest {
     @Test
     void returnNullIfStatusCodeOfLinkIs400Or500() {
 
-        LinkDialogHandler linkDialogHandler = Mockito.spy(LinkDialogHandler.class);
+        final LinkDialogHandler linkDialogHandler = Mockito.spy(LinkDialogHandler.class);
 
         Mockito.when(linkDialogHandler.checkStatusCodeOfLink(Mockito.anyString())).thenReturn(400);
 
         final MetadataStorageRecord metadataStorageRecord =
                 linkDialogHandler.checkForMetaData("https://www.goasdogle.com");
-        assertNull(metadataStorageRecord);
+        Assertions.assertNull(metadataStorageRecord);
     }
 
     @Test
     void checkIfStatusCodeIs1000() {
 
-        LinkDialogHandler linkDialogHandler = Mockito.spy(new LinkDialogHandler());
+        final LinkDialogHandler linkDialogHandler = Mockito.spy(new LinkDialogHandler());
         final int statusCodeOfLink =
                 linkDialogHandler.checkStatusCodeOfLink("https://www.googsssle.com");
 
-        assertEquals(1000, statusCodeOfLink);
+        Assertions.assertEquals(1000, statusCodeOfLink);
     }
 
     @Test
     void checkIfStatusCodeIs200() {
 
-        LinkDialogHandler linkDialogHandler = Mockito.spy(new LinkDialogHandler());
+        final LinkDialogHandler linkDialogHandler = Mockito.spy(LinkDialogHandler.class);
+
+        Mockito.when(linkDialogHandler.checkStatusCodeOfLink(Mockito.anyString())).thenReturn(200);
         final int statusCodeOfLink =
                 linkDialogHandler.checkStatusCodeOfLink("https://www.google.com");
 
-        assertEquals(200, statusCodeOfLink);
+        Assertions.assertEquals(200, statusCodeOfLink);
     }
 }
